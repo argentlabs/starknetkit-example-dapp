7cc877e7102408c98b6c356271803af572fa2927
bluecco <bluecco@gmail.com>
2024-07-24 13:32:43 +0200

    chore: add change account and network

--- src/state/connectedWalletStarknetkitNext.ts
+++ src/state/connectedWalletStarknetkitNext.ts
@@ -1,4 +1,11 @@
+import {
+  AccountChangeEventHandler,
+  ChainId,
+  NetworkChangeEventHandler,
+} from "@starknet-io/types-js"
+import { useAtom, useAtomValue, useSetAtom } from "jotai"
 import { atomWithReset } from "jotai/utils"
+import { useEffect } from "react"
 import { ConnectorData, StarknetWindowObject } from "starknetkit-next"
 import { Connector } from "starknetkit-next"
 
@@ -6,11 +13,45 @@ export const walletStarknetkitNextAtom = atomWithReset<
   StarknetWindowObject | null | undefined
 >(undefined)
 
-// TODO: export ConnectorData from starknetkit
-/* {
-  account?: string
-  chainId?: Connector
-} | null */
 export const connectorDataAtom = atomWithReset<ConnectorData | null>(null)
 
 export const connectorAtom = atomWithReset<Connector | null>(null)
+
+export const useWalletAccountChange = () => {
+  const wallet = useAtomValue(walletStarknetkitNextAtom)
+  const setConnectorData = useSetAtom(connectorDataAtom)
+
+  const accountChangeHandler: AccountChangeEventHandler = (
+    accounts?: string[],
+  ) => {
+    setConnectorData((prev) => ({
+      account: accounts?.[0],
+      chainId: prev?.chainId,
+    }))
+  }
+  const networkChangeHandler: NetworkChangeEventHandler = async (
+    chainId?: ChainId,
+    accounts?: string[],
+  ) => {
+    let walletAccount = undefined
+    if (!accounts || accounts.length === 0) {
+      walletAccount = await wallet?.request({
+        type: "wallet_requestAccounts",
+      })
+    }
+
+    setConnectorData({
+      account: accounts?.[0] || walletAccount?.[0],
+      chainId: chainId ? BigInt(chainId) : undefined,
+    })
+  }
+
+  wallet?.on("accountsChanged", accountChangeHandler)
+  wallet?.on("networkChanged", networkChangeHandler)
+
+  useEffect(() => {
+    wallet?.off("accountsChanged", accountChangeHandler)
+    wallet?.off("networkChanged", networkChangeHandler)
+    return
+  }, [])
+}
