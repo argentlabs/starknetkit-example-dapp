5c50e86af545095394420d5df55fff5bae4c3f5e
Aleksandar Cakalic <aleksandar@argent.xyz>
2024-11-13 14:18:39 +0100

    chore: add `BraavosMobileConnector`

--- src/helpers/connectorsNext.ts
+++ src/helpers/connectorsNext.ts
@@ -3,7 +3,9 @@ import {
   isInArgentMobileAppBrowser,
   ArgentMobileConnector,
 } from "starknetkit-next/argentMobile"
-import { InjectedConnector } from "starknetkit-next/injected"
+import { ArgentX } from "starknetkit-next/argentX"
+import { Braavos } from "starknetkit-next/braavos"
+import { BraavosMobileConnector } from "starknetkit-next/braavosMobile"
 import { WebWalletConnector } from "starknetkit-next/webwallet"
 import { Argent } from "starknetkit-next/argent"
 
@@ -25,8 +27,9 @@ export const availableConnectors = isInArgentMobileAppBrowser()
           chainId: CHAIN_ID,
         },
       }),
-      new InjectedConnector({ options: { id: "argentX" } }),
-      new InjectedConnector({ options: { id: "braavos" } }),
+      new ArgentX(),
+      new Braavos(),
+      BraavosMobileConnector.init(),
       ArgentMobileConnector.init({
         options: {
           url: typeof window !== "undefined" ? window.location.href : "",
