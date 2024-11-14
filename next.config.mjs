4ef6c84377daf18b97b2d36a6ee17e9e17916f2d
Aleksandar Cakalic <aleksandar@argent.xyz>
2024-11-14 13:14:36 +0100

    feat: add script for hot reloading local packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 import p from "./package.json" assert { type: "json" }
+import path from "path"
+import { fileURLToPath } from "url"
 
 const starknetkitNextVersion = Object.entries(p.dependencies)
   .find((dep) => dep[0] === "starknetkit-next")[1]
@@ -24,6 +26,13 @@ const starknetReactNextVersion = Object.entries(p.dependencies)
   .replace("npm:", "")
   .split("@")[2]
 
+const localPackages = [
+  {
+    name: "starknetkit-next",
+    path: "../starknetkit",
+  },
+]
+
 const nextConfig = {
   productionBrowserSourceMaps: true,
   env: {
@@ -31,6 +40,44 @@ const nextConfig = {
     starknetkitLatestVersion,
     starknetReactVersion,
     starknetReactNextVersion,
+  },
+  webpack: (config, { dev, webpack }) => {
+    if (!dev) {
+      return config
+    }
+
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...localPackages.reduce(
+        (aliases, pkg) => ({
+          ...aliases,
+          [pkg.name]: path.resolve(__dirname, pkg.path),
+          [`${pkg.name}/(.*)`]: path.resolve(__dirname, `${pkg.path}/$1`),
+        }),
+        {},
+      ),
+    }
+
+    localPackages.forEach((pkg) => {
+      config.plugins.push(
+        new webpack.NormalModuleReplacementPlugin(
+          new RegExp(`^${pkg.name}(\\/.*)?$`),
+          (resource) => {
+            const requestPath = resource.request.replace(pkg.name, "")
+            resource.request = path.resolve(
+              __dirname,
+              `${pkg.path}${requestPath}`,
+            )
+            resource.context = path.dirname(resource.request)
+          },
+        ),
+      )
+    })
+
+    return config
   },
 }
 
