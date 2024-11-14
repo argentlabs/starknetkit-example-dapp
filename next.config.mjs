67be02151813c5067ac85fc891a273cc8f31b284
Aleksandar Cakalic <aleksandar@argent.xyz>
2024-11-14 13:23:29 +0100

    fix: skip hot reloading packages that are not locally imported

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 import p from "./package.json" assert { type: "json" }
 import path from "path"
 import { fileURLToPath } from "url"
+import { createRequire } from "module"
+
+const require = createRequire(import.meta.url)
 
 const starknetkitNextVersion = Object.entries(p.dependencies)
   .find((dep) => dep[0] === "starknetkit-next")[1]
@@ -48,20 +51,27 @@ const nextConfig = {
 
     const __filename = fileURLToPath(import.meta.url)
     const __dirname = path.dirname(__filename)
+    const packageJson = require(path.join(__dirname, "./package.json"))
 
     config.resolve.alias = {
       ...config.resolve.alias,
-      ...localPackages.reduce(
-        (aliases, pkg) => ({
-          ...aliases,
-          [pkg.name]: path.resolve(__dirname, pkg.path),
-          [`${pkg.name}/(.*)`]: path.resolve(__dirname, `${pkg.path}/$1`),
-        }),
-        {},
-      ),
+      ...localPackages.reduce((aliases, pkg) => {
+        if (packageJson.dependencies[pkg.name].startsWith("file:")) {
+          return {
+            ...aliases,
+            [pkg.name]: path.resolve(__dirname, pkg.path),
+            [`${pkg.name}/(.*)`]: path.resolve(__dirname, `${pkg.path}/$1`),
+          }
+        }
+
+        return { ...aliases }
+      }, {}),
     }
 
     localPackages.forEach((pkg) => {
+      if (!packageJson.dependencies[pkg.name].startsWith("file:")) {
+        return
+      }
       config.plugins.push(
         new webpack.NormalModuleReplacementPlugin(
           new RegExp(`^${pkg.name}(\\/.*)?$`),
